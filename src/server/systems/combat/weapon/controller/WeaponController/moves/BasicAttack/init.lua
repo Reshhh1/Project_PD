0d7bfccc379c425d2725245ba42d62525c7ac633
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-07-11 22:20:44 +0200

    Improved / implemented combo related logic

--- src/server/systems/combat/weapon/controller/WeaponController/moves/BasicAttack/init.lua
+++ src/server/systems/combat/weapon/controller/WeaponController/moves/BasicAttack/init.lua
@@ -9,6 +9,7 @@ local WeaponTypes = require(ReplicatedStorage.Core.systems.combat.weapon.types.W
 
 local BasicAttack = {}
 
+-- Being used in the initilization of skills
 BasicAttack.MoveName = "Basic attack"
 
 function BasicAttack.init(player: Player, charactersInHitbox, config: WeaponTypes.WeaponMoveType)
@@ -16,7 +17,6 @@ function BasicAttack.init(player: Player, charactersInHitbox, config: WeaponType
 	local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
     local comboCount = ComboModule.get(character)
     
-    print("SLAAT")
 	if humanoidRootPart then
 		for _, targetCharacter in pairs(charactersInHitbox) do
 			HitboxHandler.handleCharactersInHitbox(humanoidRootPart, targetCharacter, function()  onHit(targetCharacter, config.BASE_DAMAGE[comboCount]) end)
@@ -31,7 +31,6 @@ end
 function onHit(character: CharacterMesh, damage: number)
     local humanoid = character:FindFirstChild("Humanoid") :: Humanoid
     if humanoid then
-        print(damage)
         humanoid:TakeDamage(damage)
     end
 end
--- src/shared/systems/combat/weapon/configs/StaffWeaponConfig.lua
+++ src/shared/systems/combat/weapon/configs/StaffWeaponConfig.lua
@@ -10,8 +10,8 @@ return {
 		COMBAT = {
 			NORMAL_ATTACK = {
                 NAME = "Basic attack",
-				BASE_DAMAGE = 5,
-				COOLDOWNS = 0.5,
+				BASE_DAMAGE = { 5, 5, 5, 5, 10},
+				COOLDOWNS = { 0.5, 0.5, 0.5, 0.5, 2},
 				HITBOX_SIZE = Vector3.new(6, 6, 6),
 				HITBOX_OFFSET = CFrame.new(0, 0, -3),
 			},
