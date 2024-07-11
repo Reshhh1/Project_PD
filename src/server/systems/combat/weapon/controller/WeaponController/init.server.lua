4f2ceec2a3cf500992cae3eceddbd3de3b5c16f9
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-07-11 22:19:51 +0200

    Seperated move logic from controller

--- src/server/systems/combat/weapon/controller/WeaponController/init.server.lua
+++ src/server/systems/combat/weapon/controller/WeaponController/init.server.lua
@@ -1,38 +1,27 @@
-local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local WeaponConfigHandler = require(ReplicatedStorage.Core.systems.combat.weapon.handlers.WeaponConfigHandler)
-local HitboxHandler = require(ServerScriptService.Core.handlers.HitboxHandler)
-local CooldownModule = require(ServerScriptService.Core.utility.Cooldown)
-
-local validateArguments = require(script.validateArguments)
-local validateAttack = require(script.validateAttack)
+local validateArguments = require(script.validation.validateArguments)
+local validateAttack = require(script.validation.validateAttack)
 
 local Remotes = ReplicatedStorage.Core.systems.combat.weapon.remotes
 
-local function onHit(character: CharacterMesh, damage: number)
-    local humanoid = character:FindFirstChild("Humanoid") :: Humanoid
-    if humanoid then
-        humanoid:TakeDamage(damage)
-    end
+local function initilizeSkill(player: Player, action: string, charactersInHitbox: { Model }, config)
+	for _, module in pairs(script.moves:GetChildren()) do
+		if not module:IsA("ModuleScript") then return end
+		module = require(module)
+		if module.MoveName and module.MoveName == action and module.init then
+			module.init(player, charactersInHitbox, config)
+		end
+	end
 end
 
-Remotes.WeaponAttack.OnServerInvoke = function(player: Player, weapon: Tool, action: string, charactersInHitbox: { CharacterMesh }): any
+Remotes.WeaponAttack.OnServerInvoke = function(player: Player, weapon: Tool, action: string, charactersInHitbox: { Model }): any
 	if not validateArguments(weapon, action, charactersInHitbox) then return end
-	if not validateAttack(weapon, player) then return end
-
+	if not validateAttack(weapon, player, action) then return end
+	
 	local config = WeaponConfigHandler.getCombatMoveConfig(weapon.Name, action)
 	if not config then return end
 
-	local character = player.Character
-	local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
-	
-	if humanoidRootPart then
-		for _, targetCharacter in pairs(charactersInHitbox) do
-			HitboxHandler.handleCharactersInHitbox(humanoidRootPart, targetCharacter, function()  onHit(targetCharacter, config.BASE_DAMAGE) end)
-		end
-	end
-	CooldownModule
-		.new(player.UserId, config.NAME, 0.5)
-		:create()
+	initilizeSkill(player, action, charactersInHitbox, config)
 end
