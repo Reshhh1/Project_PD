28c2a8831e0a25db0e37e6b6c37bb4f75f72a75b
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-07-13 16:55:16 +0200

    Improved code

--- src/server/systems/combat/modules/ComboModule.lua
+++ src/server/systems/combat/modules/ComboModule.lua
@@ -1,7 +1,7 @@
 local ComboModule = {}
 
 function ComboModule.get(character: Model) 
-    local comboCount = character:GetAttribute("ComboCount")
+    local comboCount = getComboCount(character)
     if not comboCount then 
         setComboCount(character, 1)
         return 1
@@ -12,6 +12,10 @@ end
 function ComboModule.update(character: Model, maxComboCount)
     local comboCount = ComboModule.get(character)
     setComboCount(character, (comboCount % maxComboCount) + 1)
+end
+
+function getComboCount(character: Model)
+    return character:GetAttribute("ComboCount")
 end
 
 function setComboCount(character: Model, count: number)
