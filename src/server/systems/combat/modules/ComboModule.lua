f48e00b31fabc42537b93be485468a754ab798e9
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-07-13 18:05:49 +0200

    Implemented combo reset logic

--- src/server/systems/combat/modules/ComboModule.lua
+++ src/server/systems/combat/modules/ComboModule.lua
@@ -1,3 +1,7 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ComboRemote = ReplicatedStorage.Core.remotes.getComboCount
+
 local ComboModule = {}
 
 function ComboModule.get(character: Model) 
@@ -12,6 +16,18 @@ end
 function ComboModule.update(character: Model, maxComboCount)
     local comboCount = ComboModule.get(character)
     setComboCount(character, (comboCount % maxComboCount) + 1)
+    setLastAttack(character, os.clock())
+end
+
+function ComboModule.getCurrentCombo(character: Model)
+    local lastAttack = getLastAttack(character)
+    if lastAttack == nil then return ComboModule.get(character) end
+    if os.clock() > (lastAttack + 2 ) then
+        setComboCount(character, 1)
+        return 1
+    else
+        return ComboModule.get(character)
+    end
 end
 
 function getComboCount(character: Model)
@@ -21,5 +37,22 @@ end
 function setComboCount(character: Model, count: number)
     character:SetAttribute("ComboCount", count)    
 end
+
+function getLastAttack(character: Model)
+    return character:GetAttribute("LastAttack")    
+end
+
+function setLastAttack(character: Model, time)
+    character:SetAttribute("LastAttack", time)    
+end
+
+function setupRemotes()
+    ComboRemote.OnServerInvoke = function(player: Player): any
+        local character = player.Character
+        return ComboModule.getCurrentCombo(character)
+    end
+end
+
+setupRemotes()
 
 return ComboModule 
