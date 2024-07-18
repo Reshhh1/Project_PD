6e6655a8bde4554f73f13c74e4ee128882e38c8b
Reshhh1 <reshwanbarhoe1@gmail.com>
2024-07-18 20:16:49 +0200

    Changed config

--- src/shared/systems/combat/weapon/configs/StaffWeaponConfig.lua
+++ src/shared/systems/combat/weapon/configs/StaffWeaponConfig.lua
@@ -10,7 +10,7 @@ return {
 		COMBAT = {
 			NORMAL_ATTACK = {
                 NAME = "Basic attack",
-				BASE_DAMAGE = { 5, 5, 5, 5, 10},
+				BASE_DAMAGE = { 50, 5, 5, 5, 10},
 				COOLDOWNS = { 0.5, 0.5, 0.5, 0.5, 2},
 				HITBOX_SIZE = Vector3.new(6, 6, 6),
 				HITBOX_OFFSET = CFrame.new(0, 0, -3),
@@ -27,12 +27,13 @@ return {
 	{
 		ID = SPEAR_ID .. "2",
 		BASE_INFO = {
-			NAME = "E Staff",
+			NAME = "Bo Staff",
 		},
 		COMBAT = {
 			NORMAL_ATTACK = {
-				BASE_DAMAGE = 5,
-				COOLDOWNS = 0.5,
+                NAME = "Basic attack",
+				BASE_DAMAGE = { 5, 5, 5, 5, 10},
+				COOLDOWNS = { 0.5, 0.5, 0.5, 0.5, 2},
 				HITBOX_SIZE = Vector3.new(6, 6, 6),
 				HITBOX_OFFSET = CFrame.new(0, 0, -3),
 			},
