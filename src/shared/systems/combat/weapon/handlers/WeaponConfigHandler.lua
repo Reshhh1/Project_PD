8ff44b65aa2d10ae0d01519cf5d199cdb7865c16
Reshhh1 <reshwanbarhoe1@gmail.com>
2025-01-07 14:45:42 +0100

    Removed redundant code

--- src/shared/systems/combat/weapon/handlers/WeaponConfigHandler.lua
+++ src/shared/systems/combat/weapon/handlers/WeaponConfigHandler.lua
@@ -2,31 +2,17 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local ModuleFinder = require(ReplicatedStorage.Core.utility.ModuleFinder)
 
-local WeaponContainer = script.Parent.Parent.WeaponController
-local SpecialMovesContainer = script.Parent.Parent.SpecialMoves
+local TechniqueContainer = script.Parent.Parent.TechniqueController
 
 local ModuleContainer = {}
 
-local WeaponConfigHandler = {}
+local TechniqueConfigurationHandler = {}
 
-function WeaponConfigHandler.getDefaultWeaponMoveConfigByName(weaponName: string, moveName: string)
-    if not ModuleContainer[weaponName] then
-        ModuleContainer[weaponName] = require(ModuleFinder.findModuleByName(weaponName, WeaponContainer).Config)
-    end
-    for _, key in pairs(ModuleContainer[weaponName]) do
-        if not key.NAME then return end
-        if key.NAME == moveName then
-            return key
-        end
-    end
-    return nil
-end
-
-function WeaponConfigHandler.getSpecialWeaponMoveConfigByName(name: string)
+function TechniqueConfigurationHandler.getByName(name: string)
     if not ModuleContainer[name] then
-        ModuleContainer[name] = ModuleFinder.findModuleByName(name, SpecialMovesContainer)
+        ModuleContainer[name] = require(ModuleFinder.findModuleByName(name, TechniqueContainer).Config)
     end
     return ModuleContainer[name]
 end
 
-return WeaponConfigHandler
+return TechniqueConfigurationHandler
