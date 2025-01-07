9596477eb15e95f067e986421be42b859d57fa25
Reshhh1 <reshwanbarhoe1@gmail.com>
2025-01-07 16:12:37 +0100

    Changed cooldowns

--- src/shared/systems/combat/weapon/TechniqueController/BasicAttack/Config.lua
+++ src/shared/systems/combat/weapon/TechniqueController/BasicAttack/Config.lua
@@ -2,7 +2,7 @@ return {
     NAME = "BasicAttack",
     BASE_DAMAGE = { 5, 5, 5, 5, 10},
     COOLDOWNS = {
-        server = { 0.3, 0.3, 0.3, 0.3, 1.5 },
+        server = { 0.1, 0.1, 0.1, 0.1, 1.5 },
         client = 0.1
     },
     HITBOX_SIZE = Vector3.new(6, 6, 6),
