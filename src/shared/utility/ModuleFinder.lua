49bdf9effcee1b9526283c2a763b81fcaf33254e
Reshhh1 <reshwanbarhoe1@gmail.com>
2025-01-07 12:26:12 +0100

    Removed logic from global util method

--- src/shared/utility/ModuleFinder.lua
+++ src/shared/utility/ModuleFinder.lua
@@ -4,8 +4,7 @@ function ModuleFinder.findModuleByName(name: string, container: Instance)
     for _, module in pairs(container:GetChildren()) do
         if not module:IsA("ModuleScript") then continue end
         if module.Name == name then
-            local weaponConfig = require(module.Config)
-            return weaponConfig
+            return module
         end
     end
     return nil
